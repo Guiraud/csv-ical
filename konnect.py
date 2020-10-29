d31c6aab07d996fb4d4b3cd46ace4449460ae7f4
Mehdi Guiraud <mehdi.guiraud@enercoop.org>
2020-10-29 15:50:59 +0100

    mise en place de Keyring

--- konnect.py
+++ konnect.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 import keyring
+import configparser
+config = configparser.ConfigParser()
+
+
 
 
 def define_local_passwords():
     """
     Description : définition des mots de passe locaux
     """
-
+    config.read('/etc/config.python')
+    for section in config.sections():
+	    for clé,valeur in config.items(section):
+		    keyring.set_password(section,clé,valeur.strip('\"'))
+    
     logins = {}
     logins['pass'] = keyring.get_password("name",
                                           "pass")
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import konnect
 from konnect import define_local_passwords
 connect = define_local_passwords()
 print(str(connect['pass'])) 
